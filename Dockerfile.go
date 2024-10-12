ccd844ac888979ef9c848fc5d774192ba02121c9
Diana Alfaro <dicaalba@gmail.com>
2024-10-12 12:03:17 -0500

    fix: go app

--- Dockerfile.go
+++ Dockerfile.go
@@ -2,12 +2,16 @@
 FROM golang:1.19-alpine
 
 # Establecemos el directorio de trabajo
-WORKDIR /go
+WORKDIR /go/src/app
 
 # Copiamos los archivos necesarios
+COPY go/go.mod go.mod
 COPY go/main.go main.go
 
 # Instalamos las dependencias
+RUN go mod download
+
+# Compilamos la aplicación
 RUN go build -o main .
 
 # Exponer el puerto 8081 
